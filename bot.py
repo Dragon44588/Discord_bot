3de640e0788669ce644e23ddbcc1c29cd54efae6
Dragon44588 <105646122+Dragon44588@users.noreply.github.com>
2022-06-09 20:51:08 -0400

    updated main bot file
    added "if name main",
    added logging,
    added json for future use

--- bot.py
+++ bot.py
@@ -4,19 +4,27 @@ import os
 from music_cog import music_cog
 from help_cog import help_cog
 from dotenv import load_dotenv
+import json
+import logging
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+def log():
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
 
-bot = commands.Bot(command_prefix="!")
+if __name__ == "__main__":
+    log()
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    bot = commands.Bot(command_prefix="!")
+    bot.remove_command("help")
+    bot.add_cog(help_cog(bot))
+    bot.add_cog(music_cog(bot))
+    bot.run(TOKEN)
 
-bot.remove_command("help")
 
-bot.add_cog(help_cog(bot))
-bot.add_cog(music_cog(bot))
-
-bot.run(TOKEN)
-
-
-async def on_ready(self):
-    print("bot is online")
+@bot.event
+async def on_ready():
+    print("Bot online")
