77269f38eb5c8ea379cc1b68b966d9fd46cc9c8f
Dragon44588 <adrianvergan@outlook.com>
2022-06-14 16:00:52 +1000

    Update general_cog.py
    added on_message event

--- general_cog.py
+++ general_cog.py
@@ -20,3 +20,9 @@ def error_log():
     handler = logging.FileHandler(filename= file, encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
+
+
+@bot.event
+async def on_message(message):
+    print(message)
+    await bot.process_commands(message)
