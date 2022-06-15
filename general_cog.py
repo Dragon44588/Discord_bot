19cd40a53b49b977c683aa0b6f152b399684e033
Dragon44588 <adrianvergan@outlook.com>
2022-06-15 10:42:20 +1000

    updated files
    updated create_dir()
    moved the info and error functions into create_dir

--- general_cog.py
+++ general_cog.py
@@ -4,35 +4,40 @@ from datetime import datetime
 from discord.ext import commands
 import json
 import os
-
-def create_dir():
-    directory = os.path.dirname(__file__)
-    errorfile = os.path.join(directory, "/error")
-    infofile = os.path.join(directory, "/info")
-    if not os.path.exists(errorfile):
-        os.makedirs(errorfile)
-    if not os.path.exists(infofile):
-        os.makedirs(infofile)
-        
+  
 
 def handle_message(message):
     print(message.content)
 
 def info_log():
-    file = '/home/dragon44588/Desktop/Discord_bot-main/{}-info.log'.format(datetime.date(datetime.now()))
+    infofile = os.path.join(os.path.dirname(__file__), "/info/{}-info.log".format(datetime.date(datetime.now())))
     logger = logging.getLogger('discord')
     logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(filename=file, encoding='utf-8', mode='w')
+    handler = logging.FileHandler(filename=infofile, encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)    
 
 
 def error_log():
-    file = '/home/dragon44588/Desktop/Discord_bot-main/error/{}-error.log'.format(datetime.date(datetime.now()))
+    errorfile = os.path.join(os.path.dirname(__file__), "/error/{}-error.log".format(datetime.date(datetime.now())))
     logger = logging.getLogger('discord')
     logger.setLevel(logging.WARNING)
-    handler = logging.FileHandler(filename= file, encoding='utf-8', mode='w')
+    handler = logging.FileHandler(filename= errorfile, encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
 
 
+def create_dir():
+    directory = os.path.dirname(os.path.abspath(__file__))
+    errorfile = os.path.join(directory, "error")
+    infofile = os.path.join(directory, "info")
+    if not os.path.exists(errorfile):
+        print("creating errorfile at: {}".format(errorfile))
+        os.makedirs(errorfile)
+        
+    if not os.path.exists(infofile):
+        print("creating info file at: {}".format(infofile))
+        os.makedirs(infofile)
+        
+    info_log()
+    error_log()      
